6f2c13688f0d411a19edc5982b584a4f598d3bda
ramdanisalah67 <ramdanisalah67@gmail.com>
2024-05-31 09:43:39 +0100

    update v3

--- src/app/choice-operation/choice-operation.component.ts
+++ src/app/choice-operation/choice-operation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-choice-operation',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoiceOperationComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {}
 
+
+
+  goToAddClient(){
+    this.router.navigate(['addClient'])
+  }
 }
